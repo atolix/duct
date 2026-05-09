0ce01334d8d3f6f0670b1c9c001333e9102b01ad
atolix <82761106+atolix@users.noreply.github.com>
2026-05-09 17:06:35 +0900

    scan dir and file size

--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
-	currentPath := "."
+	target := "."
 
-	if len(os.Args) > 1 {
-		currentPath = os.Args[1]
+	files, err := os.ReadDir(target)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
 	}
 
-	fmt.Println("scan target:", currentPath)
+	for _, f := range files {
+		path := filepath.Join(target, f.Name())
+
+		if f.IsDir() {
+			fmt.Println("[DIR]", path)
+		} else {
+			info, err := f.Info()
+			if err != nil {
+				continue
+			}
+			fmt.Println(info.Size(), path)
+		}
+	}
 }
