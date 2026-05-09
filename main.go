2c616ad9be3a08226a1958175ec5fc7385dc6a52
atolix <82761106+atolix@users.noreply.github.com>
2026-05-09 17:31:12 +0900

    unit print

--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 )
+
+type Entry struct {
+	Path string
+	Size int64
+}
 
 func main() {
 	target := "."
@@ -15,19 +21,36 @@ func main() {
 		return
 	}
 
+	var entries []Entry
+
 	for _, f := range files {
 		path := filepath.Join(target, f.Name())
 
 		if f.IsDir() {
 			size := dirSize(path)
-			fmt.Println(size, path)
+			entries = append(entries, Entry{
+				Path: path,
+				Size: size,
+			})
 		} else {
 			info, err := f.Info()
 			if err != nil {
 				continue
 			}
-			fmt.Println(info.Size(), path)
+			size := info.Size()
+			entries = append(entries, Entry{
+				Path: path,
+				Size: size,
+			})
 		}
+	}
+
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].Size > entries[j].Size
+	})
+
+	for _, e := range entries {
+		fmt.Println(humanize(e.Size), e.Path)
 	}
 }
 
@@ -46,4 +69,17 @@ func dirSize(path string) int64 {
 	})
 
 	return size
+}
+
+func humanize(size int64) string {
+	const unit = 1024
+	if size < unit {
+		return fmt.Sprintf("%dB", size)
+	}
+	div, exp := int64(unit), 0
+	for n := size / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f%cB", float64(size)/float64(div), "KMGTPE"[exp])
 }
