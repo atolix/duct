18cf9495d480a254f60c426216442ce6c6f5e1b0
atolix <82761106+atolix@users.noreply.github.com>
2026-05-09 16:58:48 +0900

    minimal scan

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("it works.")
+	currentPath := "."
+
+	if len(os.Args) > 1 {
+		currentPath = os.Args[1]
+	}
+
+	fmt.Println("scan target:", currentPath)
 }
